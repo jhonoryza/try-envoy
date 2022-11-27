f2fefa7bb590b1cd160864033827917faaa181ad
fajar sidik priatna <jardik.oryza@gmail.com>
2022-11-27 22:56:46 +0700

    new

--- Envoy.blade.php
+++ Envoy.blade.php
@@ -1,13 +1,47 @@
-@servers([
-    'web' => ['root@8.219.208.17']
-])
+@servers(['production' => ['root@8.219.208.17']])
  
-@task('deploy', ['on' => 'web'])
-    cd /root/www/envoy.labkita.my.id
- 
-    @if ($branch)
-        git pull origin {{ $branch }}
-        composer update
-    @endif
- 
+@setup
+    $repo = 'https://github.com/jhonoryza/try-envoy.git';
+    $appDir = '/var/www';
+    $branch = 'master';
+
+    date_default_timezone_set('Asia/Jakarta');
+    $date = date('YmdHis');
+
+    $builds = $appDir . '/sources';
+    $deployment = $builds . '/' . $date;
+
+    $serve = $appDir . '/source';
+    $env = $appDir . '/.env';
+    $storage = $appDir . '/storage';
+@endsetup
+
+@story('deploy')
+    git
+    install
+    live
+@endstory
+
+@task('git', ['on' => 'production'])
+    git clone -b {{ $branch }} "{{ $repo }}" {{ $deployment }}
+@endtask
+
+@task('install', ['on' => 'production'])
+    cd {{ $deployment }}
+
+    rm -rf {{ $deployment }}/storage
+    
+    ln -nfs {{ $env }} {{ $deployment }}/.env
+    
+    ln -nfs {{ $storage }} {{ $deployment }}/storage
+
+    composer install --prefer-dist --no-dev
+    
+    php ./artisan migrate --force
+@endtask
+
+@task('live', ['on' => 'production'])
+    cd {{ $deployment }}
+    
+    ln -nfs {{ $deployment }} {{ $serve }}
 @endtask
